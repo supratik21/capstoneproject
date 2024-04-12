cbcd9ec0c350535350f73b4723223e3c9d5d29ff
Supratik Sarkar <supratiksarkar21@gmail.com>
2024-04-12 20:09:35 +1000

    Updating Chart code

--- Aged-Ticket-Analysis/src/index.js
+++ Aged-Ticket-Analysis/src/index.js
@@ -9,20 +9,42 @@ resolver.define('getAgedTicket', async (req) => {
     // Adding Custom code
     console.info(`Project Key ${req.context.extension.project.key}`);
     const project_key =  req.context.extension.project.key;
-    const jql = `project in (${project_key}) and created >= -90d and status != 'Done' order by created DESC`;
+    const jql_last_month = `project in (${project_key}) and created >= -30d and created <= -0d and status NOT IN ("Done","Completed","Resolved", "Closed") order by created DESC`;
+    const jql_last_one_to_three_months = `project in (${project_key}) and created <= -30d and created >= -90d and status NOT IN ("Done","Completed","Resolved", "Closed") order by created DESC`;
+    const jql_last_three_to_six_months = `project in (${project_key}) and created <= -90d and created >= -180d and status NOT IN ("Done","Completed","Resolved", "Closed") order by created DESC`;
+    const jql_last_six_to_nine_months = `project in (${project_key}) and created <= -180d and created >= -270d and status NOT IN ("Done","Completed","Resolved", "Closed") order by created DESC`;
+    const jql_more_than_nine_months = `project in (${project_key}) and created <= -270d and status NOT IN ("Done","Completed","Resolved", "Closed") order by created DESC`;
+
     const output = [];
-    const response_five_days = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql}`);
-    console.info(response_five_days);
-    //const data = await response_five_days.json();
+    const response_last_month = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_last_month}`);
+    const response_last_one_to_three_months = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_last_one_to_three_months}`);
+    const response_last_three_to_six_months= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_last_three_to_six_months}`);
+    const response_last_six_to_nine_months = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_last_six_to_nine_months}`);
+    const response_more_than_nine_months = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_more_than_nine_months}`);
+
+    console.info(response_last_month);
     try {
-        if (!response_five_days.ok) {
-            throw new Error(`Request failed with status ${response_five_days.status}`);
+        if (!response_last_month.ok) {
+            throw new Error(`Request failed with status ${response_last_month.status}`);
         }
-        const jsonResponse_five_days = await response_five_days.json();
-        //console.info(jsonResponse_five_days);
-        const total_five_days = jsonResponse_five_days.total;
+        const jsonResponse_last_month = await response_last_month.json();
+        const jsonResponse_last_one_to_three_months = await response_last_one_to_three_months.json();
+        const jsonResponse_last_three_to_six_months = await response_last_three_to_six_months.json();
+        const jsonResponse_last_six_to_nine_months = await response_last_six_to_nine_months.json();
+        const jsonResponse_more_than_nine_months = await response_more_than_nine_months.json();
+
+        const total_last_month = jsonResponse_last_month.total;
+        const total_last_one_to_three_months = jsonResponse_last_one_to_three_months.total;
+        const total_last_three_to_six_months = jsonResponse_last_three_to_six_months.total;
+        const total_last_six_to_nine_months = jsonResponse_last_six_to_nine_months.total;
+        const total_more_than_nine_months = jsonResponse_more_than_nine_months.total;
+
         output[0] = {
-          'five_days' : total_five_days
+          'last_month' : total_last_month,
+          'last_one_to_three_months' : total_last_one_to_three_months,
+          'last_three_to_six_month' : total_last_three_to_six_months,
+          'last_six_to_nine_months' : total_last_six_to_nine_months,
+          'more_than_nine_months' : total_more_than_nine_months,
         };
     } 
     catch (error) {
@@ -40,20 +62,46 @@ resolver.define('getAgedTicketChartData', async (req) => {
     // Adding Custom code
     console.info(`Project Key ${req.context.extension.project.key}`);
     const project_key =  req.context.extension.project.key;
-    const jql = `project in (${project_key}) AND status = "Open" AND created >= startOfMonth(-1) AND created <= endOfMonth(-1)`;
+    //const jql = `project in (${project_key}) AND status != "Resolved" AND created >= startOfMonth(-1) AND created <= endOfMonth(-1)`;
+    const jql_this_month = `project in (${project_key}) AND status NOT IN ("Done","Completed","Resolved", "Closed") AND created >= startOfMonth(0)`;
+    const jql_last_month = `project in (${project_key}) AND status NOT IN ("Done","Completed","Resolved", "Closed") AND created >= startOfMonth(-1) AND created <= startOfMonth(0)`;
+    const jql_second_last_month = `project in (${project_key}) AND status NOT IN ("Done","Completed","Resolved", "Closed") AND created >= startOfMonth(-2) AND created <= startOfMonth(-1)`;
+    const jql_third_last_month = `project in (${project_key}) AND status NOT IN ("Done","Completed","Resolved", "Closed") AND created >= startOfMonth(-3) AND created >= startOfMonth(-2)`;
+    const jql_fourth_last_month = `project in (${project_key}) AND status NOT IN ("Done","Completed","Resolved", "Closed") AND created >= startOfMonth(-4) AND created >= startOfMonth(-3)`;
+    const jql_fifth_last_month = `project in (${project_key}) AND status NOT IN ("Done","Completed","Resolved", "Closed") AND created >= startOfMonth(-5) AND created >= startOfMonth(-4)`;
+    
     const output = [];
-    const response_last_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql}`);
-    //console.info(response_five_days);
-    //const data = await response_five_days.json();
+    const response_this_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_this_month}`);
+    const response_last_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_last_month}`);
+    const response_second_last_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_second_last_month}`);
+    const response_third_last_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_third_last_month}`);
+    const response_fourth_last_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_fourth_last_month}`);
+    const response_fifth_last_month= await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql_fifth_last_month}`);
+
     try {
         if (!response_last_month.ok) {
-            throw new Error(`Request failed with status ${response_last_month.status}`);
+            throw new Error(`Request failed with status ${response_this_month.status}`);
         }
+        const jsonResponse_this_month = await response_this_month.json();
         const jsonResponse_last_month = await response_last_month.json();
-        console.info(jsonResponse_last_month);
-        const total_open_last_month = jsonResponse_last_month.total;
+        const jsonResponse_second_last_month = await response_second_last_month.json();
+        const jsonResponse_third_last_month = await response_third_last_month.json();
+        const jsonResponse_fourth_last_month = await response_fourth_last_month.json();
+        const jsonResponse_fifth_last_month = await response_fifth_last_month.json();
+        console.info(response_this_month);
+        const total_open_this_month = jsonResponse_this_month.total;
+        const total_open_last__month = jsonResponse_last_month.total;
+        const total_open_second_last_month = jsonResponse_second_last_month.total;
+        const total_open_third_last_month = jsonResponse_third_last_month.total;
+        const total_open_fourth_last_month = jsonResponse_fourth_last_month.total;
+        const total_open_fifth_last_month = jsonResponse_fifth_last_month.total;
         output[0] = {
-          'open_last_month' : total_open_last_month
+          'open_this_month' : total_open_this_month,
+          'open_last_month' : total_open_last__month,
+          'open_second_last_month' : total_open_second_last_month,
+          'open_third_last_month' : total_open_third_last_month,
+          'open_fourth_last_month' : total_open_fourth_last_month,
+          'open_fifth_last_month' : total_open_fifth_last_month,
         };
     } 
     catch (error) {
--- Aged-Ticket-Analysis/static/hello-world/src/App.js
+++ Aged-Ticket-Analysis/static/hello-world/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
-import { jsx } from '@emotion/react';
+import Chart from 'chart.js/auto';
 
 import DynamicTable from '@atlaskit/dynamic-table';
 
@@ -20,7 +20,57 @@ function App() {
             // Set an error state or display an error message to the user
         });
     }, []);
-   
+
+    useEffect(() => {
+        if (agedTicketChartData.length > 0) {
+            const currentDate = new Date();
+            const lastSixMonths = [];
+            const monthNames = [
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            ];
+
+            for (let i = 5; i >= 0; i--) {
+                const date = new Date(currentDate.getFullYear(), currentDate.getMonth() - i, 1);
+                const month = monthNames[date.getMonth()];
+                lastSixMonths.push(month);
+            }
+
+            const yValues = [
+                agedTicketChartData[0].open_fifth_last_month,
+                agedTicketChartData[0].open_fourth_last_month,
+                agedTicketChartData[0].open_third_last_month,
+                agedTicketChartData[0].open_second_last_month,
+                agedTicketChartData[0].open_last_month,
+                agedTicketChartData[0].open_this_month,
+            ];
+            const barColors = ["red", "green", "blue", "orange", "pink", "grey"];
+
+            const ctx = document.getElementById('myChart');
+            new Chart(ctx, {
+                type: "bar",
+                data: {
+                    labels: lastSixMonths,
+                    datasets: [{
+                        backgroundColor: barColors,
+                        data: yValues
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: "Aged Ticket Chart"
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+        }
+    }, [agedTicketChartData]);
+
     return (
         <div>
             <h2>Aged Ticket Manage Console</h2>
@@ -33,26 +83,31 @@ function App() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td headers="five_days">5 Days</td>
-                        <td headers="count_five_days">{agedTicketData.length > 0 ? agedTicketData[0].five_days : '0'}</td>
+                        <td headers="last_month">Last Month</td>
+                        <td headers="count_last_month">{agedTicketData.length > 0 ? agedTicketData[0].last_month : '0'}</td>
                     </tr>
                     <tr>
-                        <td headers="ten_days">10 Days</td>
-                        <td headers="count_ten_days">{agedTicketData.length > 0 ? agedTicketData[0].ten_days : '0'}</td>
+                        <td headers="last_one_to_three_months">Last 1-3 months</td>
+                        <td headers="count_last_one_to_three_months">{agedTicketData.length > 0 ? agedTicketData[0].last_one_to_three_months : '0'}</td>
                     </tr>
                     <tr>
-                        <td headers="fifteen_days">15 Days</td>
-                        <td headers="count_fifteen_days">{agedTicketData.length > 0 ? agedTicketData[0].fifteen_days : '0'}</td>
+                        <td headers="last_three_to_six_month">Last 3-6 months</td>
+                        <td headers="count_last_three_to_six_month">{agedTicketData.length > 0 ? agedTicketData[0].last_three_to_six_month : '0'}</td>
                     </tr>
                     <tr>
-                        <td headers="thirty_days">30 Days</td>
-                        <td headers="count_thirty_days">{agedTicketData.length > 0 ? agedTicketData[0].thirty_days : '0'}</td>
+                        <td headers="last_six_to_nine_months">Last 6-9 months</td>
+                        <td headers="count_last_six_to_nine_months">{agedTicketData.length > 0 ? agedTicketData[0].last_six_to_nine_months : '0'}</td>
+                    </tr>
+                    <tr>
+                        <td headers="more_than_nine_months">More than 9 months</td>
+                        <td headers="count_more_than_nine_months">{agedTicketData.length > 0 ? agedTicketData[0].more_than_nine_months : '0'}</td>
                     </tr>
                 </tbody>
             </table>
-            <p>
-                {agedTicketChartData.length > 0 ? agedTicketChartData[0].open_last_month : 'Loading...'}
-            </p>
+            <div>
+                <canvas id="myChart" style={{ width: '100%', maxWidth: '600px' }}></canvas>
+            </div>
+            {agedTicketChartData.length > 0 ? agedTicketChartData[0].open_last_month : 'Loading...'}
         </div>
     );
 }
