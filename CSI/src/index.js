8876e3c6bb684369f26ad36b3536816259efd892
sumitsuman204 <sumitsuman204@gmail.com>
2024-04-18 14:56:56 +1000

    Latest code push for CSI

--- CSI/src/index.js
+++ CSI/src/index.js
@@ -3,9 +3,6 @@ import api, {route} from '@forge/api';
 
 const resolver = new Resolver();
 const fetchNumberOfComments = async function(issueKey){
-    
-    //const data = await response.json();
-    //return data.to
     try{
         const response = await api.asApp().requestJira(route `/rest/api/3/issue/${issueKey}/comment`);
         if (!response.ok) {
@@ -13,74 +10,267 @@ const fetchNumberOfComments = async function(issueKey){
         }
         const jsonResponse = await response.json();
         const totalNumOfComments = jsonResponse.total;
+        //console.info("Comments inside function call:", totalNumOfComments);
         return totalNumOfComments;
     } catch (error) {
         console.error('Error fetching total number of comments:', error);
-        // Handle the error appropriately, e.g., return a default value or rethrow the error
+        // If unauthorized, handle specifically or retry with correct credentials
+        if (error.status === 407) {
+            console.error('Check proxy settings or credentials!');
+        }
         throw error;
     }
 }
 
 const fetchIssuesWithNumberOfComments = async function(projectKey){
-    console.info("Checking projecttttttttttt key : ", projectKey);
-    const jql1 = `project in (${projectKey}) and created >= -90d and status != 'Done' order by created DESC`;
-    console.log(jql1);
+    //const response = await api.asApp().requestJira(route `/rest/api/3/search?jql=${encodeURIComponent(jql)}`);
+    
+    // Get the current date
+    const currentDate = new Date();
+    let startAt = 0;
+    const maxResults = 100; // Adjust based on API limits and performance
+    let totalFetched = 0;
+    let isFetching = true;
     const output = [];
-    const response_five_days = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql1}`);
-    console.info("First Response: ############ ",response_five_days);
-    const jql = `project in (${projectKey})`;
-    const response = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql}`);
-    console.info("Response : ",JSON.stringify(response));
-    try {
+    // Calculate the start date for the last 6 months
+    const startDate = new Date();
+    startDate.setMonth(currentDate.getMonth() - 6);
+    startDate.setDate(1); // Start from the first day of the month
+
+    // Format the start date in JQL-compatible format (YYYY-MM-DD)
+    const startDateFormatted = startDate.toISOString().split('T')[0];
+    //console.info("Checking Start Date ########### : ", startDateFormatted);
+    //const jql = `project in (${projectKey})`;
+    // Construct the JQL query to retrieve bugs created
+    //const jql = `project = ${projectKey} AND issuetype = Bug`;
+    // Construct the JQL query to retrieve bugs created in the last 6 months
+    //const jql = `project = ${projectKey} AND issuetype in (Bug, "[System] Incident") AND created >= "${startDateFormatted}"`;
+    const jql = `project = ${projectKey} AND issuetype in ("Bug", "[System] Incident")`;
+    
+    while (isFetching) {
+    	const response = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql}&startAt=${startAt}&maxResults=${maxResults}`);
+    	if (!response.ok) {
+        	throw new Error(`Request failed with status ${response.status}`);
+    	}
+    	const jsonResponse = await response.json();
+        const issues = jsonResponse.issues;
+        const totalIssues = jsonResponse.total;
+
+        // Create an array to hold the promises for fetching comments
+        //const commentPromises = issues.map(issue => fetchNumberOfComments(issue.key));
+
+        // Wait for all comment fetch promises to resolve
+        //const commentsCounts = await Promise.all(commentPromises);
+
+        issues.forEach((issue, index) => {
+            const assignee = issue.fields.assignee;
+            const assigneeDisplayName = assignee ? assignee.displayName : 'Unassigned';
+            
+            output.push({
+                'Incident Title': issue.key,
+                'Assignee': assigneeDisplayName,
+                'IssueType': issue.fields.issuetype.name,
+                //'NumComments': commentsCounts[index] // Use the index to match comments count to the issue
+            });
+        });
+	    startAt += issues.length;
+        console.info("Checking startingAt : ",startAt);
+        if (startAt >= totalIssues) {
+            isFetching = false;
+        }
+    }
+    
+    return output;
+};
+
+const getBugCountPerAssignee = async (projectKey) => {
+    const jql = `project = ${projectKey} AND issuetype in ("Bug", "[System] Incident") AND assignee is not EMPTY`;
+    let startAt = 0;
+    const maxResults = 50; // Smaller batch size for demonstration
+    let isFetching = true;
+    const assigneeCounts = {};
+
+
+    while (isFetching) {
+        const response = await api.asApp().requestJira(route`/rest/api/3/search?jql=${jql}&startAt=${startAt}&maxResults=${maxResults}&fields=assignee`);
         if (!response.ok) {
-          throw new Error(`Request failed with status ${response.status}`);
+            throw new Error(`Request failed with status ${response.status}`);
         }
         const jsonResponse = await response.json();
-        const total = jsonResponse.total;
-        const output = [];
-        console.log(JSON.stringify(jsonResponse.issues[0].fields.displayName));
-        for (let i = 0; i < total; i++) {
-            const issueKey = jsonResponse.issues[i].key;
-            const numberOfComments = await fetchNumberOfComments(issueKey);
-            const assignee = jsonResponse.issues[i].fields.assignee;
-            const projectId = jsonResponse.issues[i].fields.project.id;
-            //console.info(projectId);
-                //const issueTypeValue =await fetchIssueTypes(projectId);
-            const issueTypeValue =jsonResponse.issues[i].fields.issuetype.name;
-            output[i] = {
-                'Incident Title': jsonResponse.issues[i].key,
-                'Assignee': assignee ? assignee.displayName : 'Unassigned',
-                //'Assignee': jsonResponse.issues[i].fields.assignee,
-                'IssueType': issueTypeValue,
-                'NumComments': numberOfComments
-            };
+        jsonResponse.issues.forEach(issue => {
+            const assigneeName = issue.fields.assignee.displayName;
+            if (assigneeCounts[assigneeName]) {
+                assigneeCounts[assigneeName] += 1;
+            } else {
+                assigneeCounts[assigneeName] = 1;
+            }
+        });
+
+
+        startAt += jsonResponse.issues.length;
+        if (startAt >= jsonResponse.total) {
+            isFetching = false;
         }
-        console.info("Value of outputttt: ", output);
-        return output;
-    } catch (error) {
-        console.error('Error fetching total number of issues:', error);
-        throw error;
     }
+
+
+    return Object.entries(assigneeCounts).map(([assignee, count]) => ({ assignee, count }));
+};
+
+const getIssuesPerMonth = async (projectKey) => {
+    const currentDate = new Date();
+    let monthsData = [];
+
+    for (let i = 0; i < 6; i++) {
+        let monthDate = new Date();
+        monthDate.setMonth(currentDate.getMonth() - i);
+
+        // Calculate the first and last day of each month
+        const firstDayOfMonth = new Date(monthDate.getFullYear(), monthDate.getMonth(), 1);
+        const lastDayOfMonth = new Date(monthDate.getFullYear(), monthDate.getMonth() + 1, 0);
+
+        // Format dates for JQL query
+        const startDate = firstDayOfMonth.toISOString().split('T')[0];
+        const endDate = lastDayOfMonth.toISOString().split('T')[0];
+
+        // Construct the JQL query
+        const jql = `project = ${projectKey} AND issuetype in ("Bug", "[System] Incident") AND created >= "${startDate}" AND created <= "${endDate}"`;
+
+        // Make API request
+        const response = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql}`);
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+        const jsonResponse = await response.json();
+
+        // Accumulate data
+        monthsData.push({
+            monthYear: `${firstDayOfMonth.getFullYear()}-${firstDayOfMonth.getMonth() + 1}`,
+            count: jsonResponse.total
+        });
+    }
+
+    // Reverse the array to have the months in chronological order
+    return monthsData.reverse();
 };
 
 
+const getResolvedIssuesPerMonth = async (projectKey) => {
+    const currentDate = new Date();
+    let monthsData = [];
 
+    for (let i = 0; i < 6; i++) {
+        let monthDate = new Date();
+        monthDate.setMonth(currentDate.getMonth() - i);
 
+        // Calculate the first and last day of each month
+        const firstDayOfMonth = new Date(monthDate.getFullYear(), monthDate.getMonth(), 1);
+        const lastDayOfMonth = new Date(monthDate.getFullYear(), monthDate.getMonth() + 1, 0);
 
-resolver.define('getText', async (req) => {
-    console.log(req);
-    const projectKey = req.context.extension.project.key; 
-    console.log("!!!!!!!!!!Lowest define!!!!!!!!!");
-    console.log(projectKey);
-    //return 'Hello, world!';
+        // Format dates for JQL query
+        const startDate = firstDayOfMonth.toISOString().split('T')[0];
+        const endDate = lastDayOfMonth.toISOString().split('T')[0];
 
-    
-    const output = await fetchIssuesWithNumberOfComments(projectKey);
-    return output;
-    
+        // Construct the JQL query to filter resolved issues
+        const jql = `project = ${projectKey} AND issuetype in ("Bug", "[System] Incident") AND resolutiondate >= "${startDate}" AND resolutiondate <= "${endDate}" AND status in (Resolved, Closed, Done, Completed)`;
+        console.info("Checking jql query for reslolved issues per month : ", jql);
+        // Make API request
+        const response = await api.asApp().requestJira(route `/rest/api/3/search?jql=${jql}`);
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+        const jsonResponse = await response.json();
 
+        // Accumulate data
+        monthsData.push({
+            monthYear: `${firstDayOfMonth.getFullYear()}-${firstDayOfMonth.getMonth() + 1}`,
+            count: jsonResponse.total
+        });
+    }
+
+    // Reverse the array to have the months in chronological order
+    return monthsData.reverse();
+};
+
+resolver.define('getResolvedIssues', async (req) => {
+    const projectKey = req.context.extension.project.key;
+    const resolvedIssuesPerMonth = await getResolvedIssuesPerMonth(projectKey);
+    console.info("Resolved issues per month data: ", resolvedIssuesPerMonth);
+    return resolvedIssuesPerMonth;
+});
+
+resolver.define('getIssuesPerMonth', async (req) => {
+    const projectKey = req.context.extension.project.key;
+    const issuesPerMonth = await getIssuesPerMonth(projectKey);
+    console.info("Issues per month data: ", issuesPerMonth);
+    return issuesPerMonth;
+});
+
+resolver.define('getBugByAssignee', async (req) => {
+    const projectKey = req.context.extension.project.key;
+    const bugPerAssignee = await getBugCountPerAssignee(projectKey);
+    console.info("Aged ticket per assignee : ",bugPerAssignee);
+    return bugPerAssignee;
 
 });
 
-export const handler = resolver.getDefinitions();
 
+resolver.define('getBugsCountPerMonth', async (req) => {
+    const projectKey = req.context.extension.project.key;
+    let monthsData = [];
+    
+    for (let i = 0; i < 6; i++) {
+        // Calculate the first and last day of each month going back from the current month
+        let date = new Date();
+        date.setMonth(date.getMonth() - i);
+        date.setDate(1);
+        const firstDayOfMonth = date.toISOString().split('T')[0];
+
+        // Adjusting the month here for correct representation
+        const monthYear = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}`; // Ensures month is in MM format
+
+
+        date.setMonth(date.getMonth() + 1);
+        date.setDate(0);
+        const lastDayOfMonth = date.toISOString().split('T')[0];
+
+        // Construct JQL query
+        const jql = `project = ${projectKey} AND issuetype in ("Bug", "[System] Incident") AND created >= "${firstDayOfMonth}" AND created <= "${lastDayOfMonth}"`;
+        
+        // Make API request
+        const response = await api.asApp().requestJira(route`/rest/api/3/search?jql=${jql}`);
+        
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+        const jsonResponse = await response.json();
+        const totalBugs = jsonResponse.total;
+
+        // Store the data in a format suitable for charting
+        monthsData.push({
+            monthYear: `${date.getFullYear()}-${date.getMonth() + 1}`,
+            count: totalBugs
+        });
+    }
+    console.info("Checking monthsData : ", monthsData);
+    // Reverse the array to have the months in chronological order
+    return monthsData.reverse();
+});
+
+resolver.define('getBugsPerAssignee', async (req) => {
+    const projectKey = req.context.extension.project.key;
+    const getBugsPerAssignee = await getBugsCountPerAssignee(projectKey);
+    console.info("Bugs per assignee : ",getBugsPerAssignee);
+    return getBugsPerAssignee;
+});
+
+
+resolver.define('getText', async (req) => {
+    //console.log(req);
+    const projectKey = req.context.extension.project.key; 
+    const output = await fetchIssuesWithNumberOfComments(projectKey);
+    //console.info("Value of outputttt: ", output);
+    return output;
+});
+
+export const handler = resolver.getDefinitions();
--- CSI/static/hello-world/src/App.js
+++ CSI/static/hello-world/src/App.js
@@ -1,42 +1,180 @@
-import React, { useEffect, useState, Fragment } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { invoke } from '@forge/bridge';
-import { Table, Head, Row, Cell, Text } from '@forge/ui';
+import Chart from 'chart.js/auto';
+import { Table, Head, Row, Cell, Text, Button } from '@forge/ui';
 
 function App() {
     const [data, setData] = useState(null);
+    const [bugsCountData, setBugsCountData] = useState([]);
+    const [bugByAssignee, setbugByAssignee] = useState([]);
+    const [bugsResolvedPerMonth, setBugsResolved] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const recordsPerPage = 20;
+
     useEffect(() => {
-        invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+        Promise.all([
+            invoke('getText', { example: 'my-invoke-variable' }),
+            invoke('getBugsCountPerMonth', {example: 'my-invoke-variable' }),
+            invoke('getBugByAssignee', { example: 'my-invoke-variable' }),
+            invoke('getResolvedIssues', { example: 'my-invoke-variable' }),
+        ]).then(([textResponse, bugsCountResponse, perAssigneeResponse, bugsResolvedMonthly]) => {
+            console.info("Fetched data: ", textResponse);
+            setData(textResponse);  
+            setBugsCountData(bugsCountResponse);  
+            setbugByAssignee(perAssigneeResponse);
+            setBugsResolved(bugsResolvedMonthly);
+        }).catch(error => {
+            console.error('Error fetching data:', error);
+        });
     }, []);
-    if (!data) {
-        return <div>Give me a minute...</div>;
+
+    useEffect(() => {
+        if (bugByAssignee.length > 0) {
+            const assigneeNames = bugByAssignee.map(data => data.assignee);
+            const ticketCounts = bugByAssignee.map(data => data.count);
+    
+            const pieChartCtx = document.getElementById('assigneePieChart').getContext('2d');
+            new Chart(pieChartCtx, {
+                type: 'pie',
+                data: {
+                    labels: assigneeNames,
+                    datasets: [{
+                        data: ticketCounts,
+                        backgroundColor: [
+                            // Add as many colors as you have assignees
+                            'red', 'blue', 'green', 'yellow', 'purple', 'orange', 'cyan'
+                        ],
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        title: {
+                            display: true,
+                        }
+                    }
+                },
+            });
+        }
+    }, [bugByAssignee]);
+    
+    useEffect(() => {
+        if (bugsCountData.length > 0 && bugsResolvedPerMonth.length > 0) {
+            const currentDate = new Date();
+            const lastSixMonths = [];
+            const monthNames = [
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            ];
+
+            for (let i = 5; i >= 0; i--) {
+                const date = new Date(currentDate.getFullYear(), currentDate.getMonth() - i, 1);
+                const month = monthNames[date.getMonth()];
+                lastSixMonths.push(month);
+            }
+
+            const yValues = bugsCountData.map(item => item.count);
+            const yValue2 = bugsResolvedPerMonth.map(item => item.count);
+            console.info("Checking yvalue2 : ", yValue2);
+            const barColors = ["red", "green", "blue", "orange", "pink", "grey"];
+
+            const ctx = document.getElementById('myChart');
+            new Chart(ctx, {
+                type: "bar",
+                data: {
+                    labels: lastSixMonths,
+                    datasets: [{
+                        label: 'Bugs Count',
+                        backgroundColor: barColors,
+                        data: yValues
+                    },
+                    {
+                        label: 'Bugs Resolved',
+                        backgroundColor: barColors,
+                        data: yValue2
+                    }
+                ]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: "CSI Chart"
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+        }
+    }, [bugsCountData, bugsResolvedPerMonth]);
+
+    if (!data || data.length === 0) {
+        return <div>Fetching data...</div>;
     }
-    const tableRows = data.map((issue, index) => (
+
+    const pageData = data ? data.slice(currentPage * recordsPerPage, (currentPage + 1) * recordsPerPage) : [];
+
+    const tableRows = pageData.map((issue, index) => (
         <tr key={index}>
             <td>{issue['Incident Title']}</td>
             <td>{issue['Assignee']}</td>
             <td>{issue['IssueType']}</td>
-            <td>{issue['NumComments']}</td>
         </tr>
     ));
-    
+    const shouldHidePrev = currentPage === 0;
+    const shouldHideNext = currentPage >= Math.ceil(data.length / recordsPerPage) - 1;
+    const handleNextPage = () => {
+        const totalPages = Math.ceil(data.length / recordsPerPage);
+        setCurrentPage(current => (current < totalPages - 1 ? current + 1 : current));
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage(current => (current > 0 ? current - 1 : 0));
+    };
+
     return (
-        <Fragment>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Incident Title</th>
-                        <th>Assignee</th>
-                        <th>Issue Type</th>
-                        <th>Number of Comments</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableRows}
-                </tbody>
-            </table>
-        </Fragment>
+        <div>
+            <div style={{ height: '20px' }}></div> 
+            <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                <div style={{ flex: 2 }}>
+                    <h3>Number of Issues vs Issue Resolved</h3>
+                    <div style={{ height: '20px' }}></div> 
+                    <canvas id="myChart" style={{ width: '100%', maxWidth: '800px' }}></canvas>
+                </div>
+
+                <div style={{ flex: 1 }}>
+                    <h3>Bugs Per Assignee</h3>
+                    <div style={{ height: '20px' }}></div> 
+                    <canvas id="assigneePieChart" style={{ width: '100%', maxWidth: '400px' }}></canvas>
+                </div>
+            </div>
+            <h3>**Bugs Count Per Month (Last 6 Months)**</h3>
+            <Table>
+                <Head>
+                    <Row>
+                        <Cell><Text>Incident Title</Text></Cell>
+                        <Cell><Text>Assignee</Text></Cell>
+                        <Cell><Text>Issue Type</Text></Cell>
+                    </Row>
+                </Head>        
+                {tableRows}
+            </Table>
+            
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', margin: '20px 0' }}>
+                {currentPage !== 0 && (
+                    <button text="Previous" onClick={handlePrevPage}>Prev.</button>
+                )}
+                {currentPage < Math.ceil(data.length / recordsPerPage) - 1 && (
+                    <button text="Next" onClick={handleNextPage}>Next</button>
+                )}
+            </div>
+        </div>
     );
-        
 }
 
 export default App;
